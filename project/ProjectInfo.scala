68b391e63a1f5a89223cb97cd720866966a5abc9
Kevin Lee <kevin.code@kevinlee.io>
2019-10-19 15:33:36 +1100

    Upgrade: sbt: 1.2.8 => 1.3.3 / libs / sbt plugins - Project config and code have been updated accordingly Added: just-fp lib

--- project/ProjectInfo.scala
+++ project/ProjectInfo.scala
@@ -11,6 +11,11 @@ object ProjectInfo {
 
   val ProjectVersion: String = "0.1.0"
 
-  val commonWarts: Seq[wartremover.Wart] = Warts.allBut(Wart.DefaultArguments, Wart.Overloading, Wart.Any, Wart.Nothing, Wart.NonUnitStatements)
+  def commonWarts(scalaBinaryVersion: String): Seq[wartremover.Wart] = scalaBinaryVersion match {
+    case "2.10" =>
+      Seq.empty
+    case _ =>
+      Warts.allBut(Wart.DefaultArguments, Wart.Overloading, Wart.Any, Wart.Nothing, Wart.NonUnitStatements)
+  }
 
 }
